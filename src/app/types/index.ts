f4ffafa4a6506e721febebb80c84b0ed7ffe3544
Darja <daria.anushova@gmail.com>
2026-06-05 16:14:09 +0200

    add Message and API types

---
+++ src/app/types/index.ts
@@ -0,0 +1,16 @@
+export interface Message {
+  _id: string;
+  message: string;
+  author: string;
+  createdAt: string;
+}
+
+export interface CreateMessageRequest {
+  message: string;
+  author: string;
+}
+
+export interface ErrorResponse {
+  error: string;
+  details: { msg: string; param: string; location: string }[];
+}
